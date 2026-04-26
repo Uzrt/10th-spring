3e749f1633e26c4110a8be253325bda97f81b290
Uzrt <worldsilvertree@gmail.com>
2026-04-26 14:17:04 +0900

    controller 작성

--- lemon/mission/src/main/java/com/springboot/mission/domain/store/entity/StoreCategory.java
+++ lemon/mission/src/main/java/com/springboot/mission/domain/store/entity/StoreCategory.java
@@ -7,6 +7,10 @@ import lombok.*;
 @Table(name = "store_category")
 @Getter @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StoreCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "store_id")
     private Store store;
