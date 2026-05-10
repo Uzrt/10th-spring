1e21ebc69db389911675225a9e22a92287708760
Uzrt <worldsilvertree@gmail.com>
2026-05-10 13:34:51 +0900

    review service 로직 작성

--- lemon/mission/src/main/java/com/springboot/mission/domain/store/repository/StoreRepository.java
+++ lemon/mission/src/main/java/com/springboot/mission/domain/store/repository/StoreRepository.java
@@ -1,4 +1,7 @@
 package com.springboot.mission.domain.store.repository;
 
-public class StoreRepository {
+import com.springboot.mission.domain.store.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
 }
