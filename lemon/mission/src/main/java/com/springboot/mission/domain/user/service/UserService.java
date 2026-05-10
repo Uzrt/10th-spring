7cd8a7929043ce0088c752a804ddf75f2db501b4
Uzrt <worldsilvertree@gmail.com>
2026-05-10 11:37:14 +0900

    User 회원가입 로직 작성

--- lemon/mission/src/main/java/com/springboot/mission/domain/user/service/UserService.java
+++ lemon/mission/src/main/java/com/springboot/mission/domain/user/service/UserService.java
@@ -1,4 +1,37 @@
 package com.springboot.mission.domain.user.service;
 
+import com.springboot.mission.domain.user.code.MemberErrorCode;
+import com.springboot.mission.domain.user.dto.UserRequestDTO;
+import com.springboot.mission.domain.user.dto.UserResponseDTO;
+import com.springboot.mission.domain.user.entity.User;
+import com.springboot.mission.domain.user.exception.UserException;
+import com.springboot.mission.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserResponseDTO.JoinResponse join(UserRequestDTO.JoinUser request) {
+
+        // 1. 중복 가입 확인
+        if (userRepository.existsByMail(request.mail())) {
+            throw new UserException(MemberErrorCode.MEMBER_ALREADY_EXISTS);
+        }
+
+        // 2. DTO -> Entity (정적 팩토리 메서드 활용)
+        User user = request.toEntity();
+
+        // 3. 저장
+        User savedUser = userRepository.save(user);
+
+        // 4. Entity -> DTO 반환
+        return UserResponseDTO.JoinResponse.from(savedUser);
+    }
 }
